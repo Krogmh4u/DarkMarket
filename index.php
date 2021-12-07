80d0005cc1d6d6314e2806b02723a5f7435df36c
Raevv <greveson.axel@gmail.com>
2021-12-07 22:13:46 +0100

    ok

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Nunito:wght@300;500&display=swap" rel="stylesheet">
 </head>
+<!-- salope -->
 <body>
 	<div class="wrapper">
 		<div class="login-form">
