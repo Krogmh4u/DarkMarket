b44a89094d71af3f3bbda6f57000d568c6b5dd3f
Raevv <greveson.axel@gmail.com>
2021-12-08 00:35:06 +0100

    Ajout weapons

---
+++ weapons.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="weapons.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Nunito:wght@300;500&display=swap" rel="stylesheet">
+	<title>Dark Market - Weapons</title>
+</head>
+<body>
+    <nav>
+		<ul class="menu">
+			<li><a class="links">Account</a></li>
+			<li><a class="log-out">Log out</a></li>
+		</ul>
+	</nav>
+    <form action="" class="search-box">
+		<input placeholder="Search" type="text" class="search-input">
+	</form>
+    <div class="filters-wrapper">
+        <button class="filter-ar">AR</button>
+        <button class="filter-smg">SMG</button>
+        <button class="filter-pistol">Pistol</button>
+        <button class="filter-tool">Tool</button>
+        <button class="filter-clear">Clear</button>
+    </div>
+    <div class="wrapper">
+        <div class="grid-layout">
+            <div class="ar"><div class="band"><p>M4A1 - 500$</p></div></div>
+            <div class="smg"><div class="band"><p>M4A1 - 500$</p></div></div>
+            <div class="pistol"><div class="band"><p>M4A1 - 500$</p></div></div>
+            <div class="tool"><div class="band"><p>M4A1 - 500$</p></div></div>
+            <div class="tool"><div class="band"><p>M4A1 - 500$</p></div></div>
+            <div class="smg"><div class="band"><p>M4A1 - 500$</p></div></div>
+        </div>
+    </div>
+</body>
+</html>
